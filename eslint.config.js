918583686458a991e4fdc41b47d462eea1b8bce9
lasse <77295879+lassejlv@users.noreply.github.com>
2025-03-29 16:28:20 +0100

    Limit ESLint scope to src directory only

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,9 @@ import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  { files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
